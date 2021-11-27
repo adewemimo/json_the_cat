8f3d9e8f3672540450db8720302f5365c7e8543f
Seun Soetan <sesoetan@gmail.com>
2021-11-27 07:22:36 -0500

    update to test breedFetcher function

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,27 +1,33 @@
 const request = require('request');
 const breed = process.argv[2];
 
-const breedFetcher = breedName => {
+const breedFetcher = (breedName, getDescriptionCallback) => {
 
-    request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, body, response) => {
-      if (error) {
-        console.log(error);
-        return error;
-      } else {  
-        const data = JSON.parse(body.body);
-        if (data.length === 0) {
-          console.log('Breed not found');
-        } else {
-          console.log('response:', response);
-          console.log('------------')
-          console.log("data: " , data[0].description);
-          return data[0].description;
-        }
-            
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, body) => {
+    if (error) {
+      console.log(error);
+      return error;
+    } else {
+      const data = JSON.parse(body.body);
+      if (data.length === 0) {
+        console.log('Breed not found');
+      } else {
+        getDescriptionCallback(data[0].description);
       }
-    });
-}
+            
+    }
+  });
+};
 
-breedFetcher(breed);
+const breedDescription = (description) => {
+  console.log('Breed Description: ', description);
+  return description;
+};
 
-module.exports = breedFetcher;
+//When calling the test file for this function, The line will give 'breed not found' output because breed is not supplied from the command line. it will however work when this file itself is called e.g. node breedFetcher.js Chartreux
+breedFetcher(breed, breedDescription);
+
+module.exports = {
+  breedFetcher,
+  breedDescription
+};
--- test/breedFetcherTest.js
+++ test/breedFetcherTest.js
@@ -1,9 +1,9 @@
-const breedFetcher = require('../breedFetcher');
-const {assert} = require('chai');
+const {breedFetcher, breedDescription} = require('../breedFetcher');
+//const {expect} = require('chai');
 
 describe('#breedFetcher', () => {
-    it('should return breed description when the breed is provided', () => {
-        const expected = 'The Chartreux is generally silent but communicative. Short play sessions, mixed with naps and meals are their perfect day. Whilst appreciating any attention you give them, they are not demanding, content instead to follow you around devotedly, sleep on your bed and snuggle with you if you’re not feeling well.';
-        assert.equal(breedFetcher('Chartreux'), expected);
-    })
-})
+  it('should return breed description when the breed is provided', async() => {
+    const breed = 'Chartreux';
+    breedFetcher(breed, breedDescription);
+  });
+});
